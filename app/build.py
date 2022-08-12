ffafae458486cb1c0ddd8028bc8584c43552f6e6
cklosters <coen@naivi.nl>
2022-08-12 16:23:28 +0200

    Updating documentation

--- app/build.py
+++ app/build.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
     # copy cname file
     copy_file("{0}/CNAME".format(get_working_dir()), "{0}/CNAME".format(get_docs_dir()))
 
-    # push to docs repo
+    # push changes to docs repo
     docs_repo = Repository(get_root_dir())
     docs_repo.push()
 
